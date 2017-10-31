b4419631d5ae1062ecd7b281a7800b5c49a0b418
Long Gao <gaolongnuaa@gmail.com>
2017-10-31 11:18:40 -0400

    Initialize the R package for ARVIN

---
+++ R/NetworkFeatureCalculation.R
@@ -0,0 +1,59 @@
+#'Compute network-based features needed for risk variant prediction
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param Nodes a list of node names 
+#'@return a list object containing the different type network features 
+NetFeature <- function(Net, Nodes){
+  
+  return(FeatureList)
+}
+
+#'Compute betweenness centrality for a list of nodes
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param Nodes a list of node names 
+#'@return a list object containing the different type network features 
+BetFeature <- function(Net, Nodes){
+  
+  return(BetList)
+}
+
+#'Compute closeness centrality for a list of nodes
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param Nodes a list of node names 
+#'@return a list object containing the different type network features 
+CloseFeature <- function(Net, Nodes){
+  
+  return(CloseList)
+}
+
+#'Compute pagerank centrality for a list of nodes
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param Nodes a list of node names 
+#'@return a list object containing the different type network features 
+PageFeature <- function(Net, Nodes){
+  
+  return(PageList)
+}
+
+#'Compute module score for a list of nodes
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param Nodes a list of node names 
+#'@return a list object containing the different type network features 
+ModuleFeature <- function(Net, Nodes){
+  
+  return(ModuleList)
+}
