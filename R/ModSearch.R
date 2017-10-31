a95a8948299befdc966d05ca2ee24f2ab5e36173
Long Gao <gaolongnuaa@gmail.com>
2017-10-31 11:52:29 -0400

    Add module search and RF training and prediction

---
+++ R/ModSearch.R
@@ -0,0 +1,228 @@
+#library("igraph")#igraph contains basic graph operations and some simple graph algorithms
+#library("Rcpp")
+
+#'Search modules from a list of seeds/nodes
+#'
+#'This function ...
+#'
+#'@param Net a graph object representing the input network
+#'@param V_weight a numeric vector of node weight
+#'@param Adj_List a list object representing the adjacency list of the input network
+#'@param E_adj the adjacency matrix
+#'@param rank_list the 
+#'@return a list object containing modules and related stattistics
+module_search_ind <- function(V_weight, Adj_List, ind_seed, E_adj, rank_list){
+  ptm_1 <- proc.time()
+  cur_seed <- ind_seed#get the seed name
+  print("current seed:")
+  print(cur_seed)
+  cur_mod <- list(mem=c(cur_seed), seed=cur_seed, score=V_weight[cur_seed], last_mem=cur_seed, neighbor=c(), score_table=c(), size=1, pval=-1, fdr=-1, state="NULL", com_size=0)#initialize a module object in list format
+  #last_mem is the last gene which was added into current module
+  #neighbor is the list of all possible neighbor genes of members in current module
+  #score table records neighbors that will be potentially added into current module and the second column is the gain of score if add this node
+  while(cur_mod$state == "NULL"){#run thAdj_List_randis loop until there's no node can be added into current module
+    #print(cur_mod$mem)
+    cand_neighbor <- c()#get all neighbors of genes in current module
+    #iteratively obtain neighbors for all genes
+    
+    cur_mem <- cur_mod$last_mem#obtain a given node
+    cur_neighbor <- names(Adj_List[[cur_mem]])
+    cand_neighbor <- union(cur_mod$neighbor, cur_neighbor)#add newly identified neighbors to the entire set	
+    cand_neighbor <- setdiff(cand_neighbor, cur_mod$mem)#get rid of nodes that are already in this module
+    cand_neighbor <- setdiff(cand_neighbor, names(cur_mod$score_table))#also get rid of potential neighbors that have been considered
+    cur_mod$neighbor <- cand_neighbor
+    
+    if(length(cand_neighbor) > 5){
+      #if the potential set is not empty, at most pick top 5
+      NW_cand <- V_weight[cand_neighbor]#get the node weight of candidate nodes
+      NW_cand <- sort(NW_cand, decreasing=T)#rank the array by node weight
+      cand_neighbor <- names(NW_cand[1:5])#only use the at most top 5 nodes
+    }
+    cur_mod <- module_update(V_weight, Adj_List, cur_mod, cand_neighbor, E_adj)
+    cur_mod$com_size <- get_com_size(cur_mod$mem, V_weight, Adj_List)
+    if(cur_mod$size > 100){
+      #if(cur_mod$com_size > 1000){
+      break
+    }
+  }
+  
+  print(cur_mod$mem)
+  print("Time to find this module:")
+  print(proc.time() - ptm_1)
+  return(cur_mod)
+  
+}
+
+##################################################################################
+#This function was designed to do module membership checking
+#Net, this is an igraph obeject with node and edge weight specified
+module_update <- function(V_weight, Adj_List, cur_mod, can_neighbor, E_adj){
+  new_mod <- cur_mod#updated module, but now it is just a copy of current module
+  #each iteration the following loop tries to update new_mod
+  neighbor_neighbor <- c()#neighbors of a given neighbor
+  if(length(can_neighbor) != 0){#if this set of neighbors is not empty
+    #score list for candidate genes only
+    add_score_list <- rep(-1, length(can_neighbor))
+    for(i in 1:length(can_neighbor)){
+      cur_nb <- can_neighbor[i]#get a given neighbor
+      neighbor_neighbor <- names(Adj_List[[cur_nb]])
+      #print(proc.time() - ptm_1)
+      con_mem <- intersect(cur_mod$mem, neighbor_neighbor)#to get overlap with module genes so we can know which ones are connected to this neighbor		
+      gain <- V_weight[cur_nb] + sum(Adj_List[[cur_nb]][con_mem])
+      add_score_list[i] <- gain
+    }
+    names(add_score_list) <- can_neighbor
+    cur_mod$score_table <- c(cur_mod$score_table, add_score_list)
+  }
+  #print("length")
+  #print(length(cur_mod$score_table))
+  if(length(cur_mod$score_table) > 0){
+    max_gain <- max(cur_mod$score_table)
+    max_index <- which.max(cur_mod$score_table)#pick the node which can introduce largest score gain
+    new_node <- names(cur_mod$score_table[max_index])#get the name of the node
+    #if(max_gain  >= 0.01 * cur_mod$score){
+    #if(max_gain  > 0){
+    #if(T){
+    hit_GO <- FALSE
+    if(cur_mod$size < 50){
+      hit_GO <- TRUE
+    }
+    else{
+      cur_overlap <- intersect(names(Adj_List[[new_node]]), cur_mod$mem)
+      if((cur_mod$score + max_gain)/(cur_mod$com_size + length(cur_overlap) + 1)  >=   cur_mod$score/cur_mod$com_size){
+        #if((cur_mod$score + max_gain)/(cur_mod$size + 1)  >=   cur_mod$score/cur_mod$size){
+        #if((cur_mod$score + max_gain)/(cur_mod$size + 1)  >=   cur_mod$score/cur_mod$size){
+        hit_GO <- TRUE
+      }
+    }
+    if(hit_GO){
+      #max_index <- which.max(cur_mod$score_table)#pick the node which can introduce largest score gain
+      #new_node <- names(cur_mod$score_table[max_index])#get the name of the node
+      max_gain <- cur_mod$score_table[max_index]#get the gain of score
+      cur_mod$mem <- c(cur_mod$mem,new_node)#update the membership
+      cur_mod$size <- cur_mod$size + 1#update the size
+      cur_mod$score <- cur_mod$score + max_gain#update the score
+      cur_mod$last_mem <- new_node#indicate which has been added
+      cur_mod$score_table <- cur_mod$score_table[-max_index]#remove that added node
+      #update the score table with regard to the new node since the topology of the module has changed
+      neighbor_neighbor <- names(Adj_List[[new_node]])
+      poten_overlap <- intersect(names(cur_mod$score_table), neighbor_neighbor)#check how many potential nodes that are connected to this newly added node, and then update their corresponding score
+      if(length(poten_overlap) > 0){
+        cur_mod$score_table[poten_overlap] <-  cur_mod$score_table[poten_overlap] + E_adj[new_node, poten_overlap]
+      }
+    }#if this module can be still updated
+    else{#there is no neighbor can satistied the criterion
+      cur_mod$state <- "Stop"#set the state flag to "stop"
+    }
+    if(length(can_neighbor) == 0 & length(cur_mod$score_table) == 0 ){
+      #there is no additional nodes that are connected to current module
+      cur_mod$state <- "Stop"#set the state flag to "stop"
+    }
+  }#check the score table length
+  else{#if the score table is already empty
+    cur_mod$state <- "Stop"
+  }
+  #print("Time for update all!")
+  #print(proc.time() - ptm_1)
+  return(cur_mod)
+}
+
+##################################################################################
+#given a list of modules
+#this function returns a matrix for pairwise jaccard index
+# cppFunction('NumericMatrix pair_jac(List mod_list){
+#             int num = mod_list.size();
+#             NumericMatrix mat(num,num);
+#             for(int i = 0; i < num - 1; ++i){
+#             CharacterVector v1 = mod_list[i];
+#             for(int j = i + 1; j < num; ++j){
+#             CharacterVector v2 = mod_list[j];
+#             IntegerVector v3 = match(v1,v2);
+#             int n = v3.size();
+#             int nx = v1.size();
+#             int ny = v2.size();
+#             int count = 0;
+#             for (int k = 0; k < n; ++k){
+#             count += 1 - IntegerVector::is_na(v3[k]);
+#             }
+#             float jac = (float)(count)/(float)(nx + ny - count);
+#             mat(i,j) = jac;
+#             mat(j,i) = jac;
+#             }
+#             }
+#             return mat;
+#             }')
+
+
+##################################################################################
+#cluster modules based on pairwise jaccard index
+merge_modules <- function(mod_sets, tree_height=0.5, V_weight, Adj_List, eSNP_seeds){
+  mod_mem <- lapply(mod_sets, function(x) x[["mem"]])#get the list of member names only
+  print("Time for running pairwise jac index: ")
+  system.time(pj <- pair_jac(mod_mem))#compute pairwise jaccard index
+  print(dim(pj))
+  print(mod_sets)
+  print("Time for clustering modules: ")
+  system.time(hc <- hclust(as.dist(1-pj)))#cluster all modules based on pairwise overlap
+  mem_clr <- cutree(hc, h = tree_height)#cut the tree by the height parameter
+  #mem_clr <- cutree(hc, k = length(mod_mem))#cut the tree by the height parameter
+  names(mem_clr) <- 1:length(mem_clr)
+  num <- max(mem_clr)#get how many modules that are still remaining
+  
+  ptm_1 <- proc.time() 
+  up_mod <- list()#list of modules after merging
+  for(i in 1:num){
+    cur_mem_list <- mod_mem[which(mem_clr==i)]
+    cur_mem <- unique(unlist(cur_mem_list))#merge all modules with relatively large overlap
+    #initialize a module
+    cur_mod <- list(mem=cur_mem, score=0,avg_score=0, last_mem="Merged", neighbor=c(), score_table=c(), size=length(cur_mem), pval=-1, fdr=-1, state="NULL", gene_num=0, com_size=0, com_score=0, gscore=0, gcomscore=0)
+    cur_mod$score <- compute_mod_score(cur_mem, V_weight, Adj_List)#compute the score for a given module
+    cur_mod$com_size <- get_com_size(cur_mem, V_weight, Adj_List)
+    cur_mod$avg_score <- cur_mod$score/cur_mod$size
+    cur_mod$com_score <- cur_mod$score/cur_mod$com_size
+    cur_mod$gene_num <- cur_mod$size - length(intersect(eSNP_seeds, cur_mem))
+    cur_mod$g_score <- cur_mod$score/cur_mod$gene_num
+    num_snp <- cur_mod$size - cur_mod$gene_num
+    cur_mod$gcomscore <- (cur_mod$score - 0.01 * num_snp)/(cur_mod$com_size - 1 * num_snp)
+    if(cur_mod$gene_num > 20){
+      up_mod <- append(up_mod, list(cur_mod))#append a new module to the updated module list
+    }	
+  }#for
+  
+  print("Time for recomputing the module score: ")
+  print(proc.time() - ptm_1)
+  return(up_mod)
+}
+
+##################################################################################
+#compute the score for a module given the set of member names, node weight and adj list
+compute_mod_score <- function(mem, V_weight, Adj_List){
+  score <- 0
+  score <- score + sum(V_weight[mem])#get all node_score
+  Sub_adj_list <- Adj_List[mem]#get subset of the adj list only for the module members only
+  edge_score <- 0
+  for(i in 1:length(mem)){
+    this_mem <- mem[i]
+    common_mem <- intersect(mem, names(Sub_adj_list[[this_mem]]))
+    edge_score <- edge_score + sum(Sub_adj_list[[this_mem]][common_mem])
+  }
+  score <- score + edge_score/2#each edge has been counted twice
+  return(score)
+}
+
+##################################################################################
+#get combine size which is the number of nodes plus number of edges
+get_com_size <- function(mem, V_weight, Adj_List){
+  com_size <- 0
+  com_size <- length(mem)#get the size of nodes
+  edge_score <- 0
+  Sub_adj_list <- Adj_List[mem]#get subset of the adj list only for the module members only
+  num_edge <- 0
+  for(i in 1:length(mem)){
+    this_mem <- mem[i]
+    common_mem <- intersect(mem, names(Sub_adj_list[[this_mem]]))
+    num_edge <- num_edge + length(common_mem)
+  }
+  com_size <- com_size + num_edge/2#each edge has been counted twice and it is shared by two nodes
+  return(com_size)
+}
---
+++ R/TrainAndPred.R
@@ -0,0 +1,226 @@
+#library("pROC")
+#library("randomForest")
+#library("caret")
+
+#'Train a classifier 
+#'
+#'This function ...
+#'
+#'@param FeatureMatrix a matrix of feature values
+#'@param pos a vector specifying the positive set of elements
+#'@param neg a vector specifying the control set of elements
+#'@return a random forest classifier
+trainMod <- function(FeatureMatrix, pos, neg){
+  
+  return(RFmodel)
+}
+
+#'Compute prediction score 
+#'
+#'This function ...
+#'
+#'@param FeatureMatrix a matrix of feature values
+#'@param RFmodel a random forest classifier
+#'@return a random forest classifier
+predMod <- function(FeatureMatrix, RFmodel){
+  return(predScore)
+}
+
+
+# #############################################
+# Features_all <- read.table("Combined_Features_avg.txt", sep = "\t", head=T)
+# Features_all[is.na(Features_all)] <- 0
+# #Features_all <- Features_all[,-c(4)]
+# #Features_all <- Features_all[, c(1:7, dim(Features_all)[2]-1, dim(Features_all)[2])]#network feature alone
+# #Features_all <- Features_all[, c(1,8:181,dim(Features_all)[2]-1, dim(Features_all)[2])]#gwawa
+# #Features_all <- Features_all[, c(1, 182:dim(Features_all)[2])]#funseq
+# 
+# snp_dis <- subset(Features_all, Features_all[,dim(Features_all)[2]] == "positive")
+# snp_rnd <- subset(Features_all, Features_all[,dim(Features_all)[2]-1] == "RND")
+# snp_tss <- subset(Features_all, Features_all[,dim(Features_all)[2]-1] == "TSS")
+# snp_1k <- subset(Features_all, Features_all[,dim(Features_all)[2]-1] == "1K")
+# snp_1k[, dim(Features_all)[2]-1] <- "X1K"
+# 
+# name_dis <- snp_dis[,1]
+# name_rnd <- snp_rnd[,1]
+# name_tss <- snp_tss[,1]
+# name_1k <- snp_1k[,1]
+# snp_dis <- snp_dis[,-c(1,dim(snp_dis)[2] - 1)]
+# snp_rnd <- snp_rnd[,-c(1,dim(snp_rnd)[2])]
+# snp_tss <- snp_tss[,-c(1,dim(snp_tss)[2])]
+# snp_1k <- snp_1k[,-c(1, dim(snp_1k)[2])]
+# colnames(snp_dis)[dim(snp_dis)[2]] <- "category"
+# colnames(snp_rnd)[dim(snp_rnd)[2]] <- "category"
+# colnames(snp_tss)[dim(snp_tss)[2]] <- "category"
+# colnames(snp_1k)[dim(snp_1k)[2]] <- "category"
+# 
+# com_rnd <- rbind(snp_dis, snp_rnd)
+# com_tss <- rbind(snp_dis, snp_tss)
+# com_1k <- rbind(snp_dis, snp_1k)
+# 
+# x <- as.matrix(com_rnd[,1:(dim(com_rnd)[2] - 1)])
+# y <- as.factor(as.character(com_rnd[,dim(com_rnd)[2]]))
+# train_control <- trainControl(method="cv", number=5,summaryFunction=twoClassSummary, classProbs=T, savePredictions = T)
+# rfFit_Com_RND <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# rfFit <- rfFit_Com_RND
+# #rfFit_Net_RND <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(100,100))
+# #rfFit <- rfFit_Net_RND
+# #rfFit_gwawa_RND <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_gwawa_RND
+# #rfFit_funseq_RND <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_funseq_RND
+# print(rfFit)
+# selectedIndices <- rfFit$pred$mtry == 6
+# ROC_Com_RND <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_Net_RND <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_gwawa_RND <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_funseq_RND <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# plot(ROC)
+# 
+# 
+# x <- as.matrix(com_tss[,1:(dim(com_tss)[2] - 1)])
+# y <- as.factor(as.character(com_tss[,dim(com_tss)[2]]))
+# train_control <- trainControl(method="cv", number=10,summaryFunction=twoClassSummary, classProbs=T, savePredictions = T)
+# #train_control <- trainControl(method="repeatedcv", number=10, repeats=3,summaryFunction=twoClassSummary, classProbs=T, savePredictions = T)
+# rfFit_Com_TSS <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# rfFit <- rfFit_Com_TSS
+# #rfFit_Net_TSS <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_Net_TSS
+# #rfFit_gwawa_TSS <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_gwawa_TSS
+# #rfFit_funseq_TSS <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_funseq_TSS
+# print(rfFit)
+# selectedIndices <- rfFit$pred$mtry == 6
+# ROC_Com_TSS <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_Net_TSS <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_gwawa_TSS <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_funseq_TSS <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# plot(ROC_funseq_TSS)
+# 
+# 
+# x <- as.matrix(com_1k[,1:(dim(com_1k)[2] - 1)])
+# y <- as.factor(as.character(com_1k[,dim(com_1k)[2]]))
+# train_control <- trainControl(method="cv", number=10,summaryFunction=twoClassSummary, classProbs=T, savePredictions = T)
+# #train_control <- trainControl(method="repeatedcv", number=10, repeats=3,summaryFunction=twoClassSummary, classProbs=T, savePredictions = T)
+# rfFit_Com_1k <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# rfFit <- rfFit_Com_1k
+# #rfFit_Net_1k <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_Net_1k
+# #rfFit_gwawa_1k <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_gwawa_1k
+# #rfFit_funseq_1k <- train(x, y, trControl=train_control, method="rf", strata=y, sampsize=c(50,50))
+# #rfFit <- rfFit_funseq_1k
+# print(rfFit)
+# selectedIndices <- rfFit$pred$mtry == 186
+# ROC_Com_1k <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_Net_1k <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_gwawa_1k <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# #ROC_funseq_1k <- roc(rfFit$pred$obs[selectedIndices], rfFit$pred$positive[selectedIndices])
+# plot(ROC)
+# 
+# 
+# 
+# #######################################################################################
+# load("Aug22_OPT_classification.RData")
+# print(rfFit_Com_RND_opt)
+# selectedIndices <- rfFit_Com_RND_opt$pred$mtry == 40
+# ROC_Com_RND <- roc(rfFit_Com_RND_opt$pred$obs[selectedIndices], rfFit_Com_RND_opt$pred$positive[selectedIndices])
+# 
+# print(rfFit_Com_TSS_opt)
+# selectedIndices <- rfFit_Com_TSS_opt$pred$mtry == 18
+# ROC_Com_TSS <- roc(rfFit_Com_TSS_opt$pred$obs[selectedIndices], rfFit_Com_TSS_opt$pred$positive[selectedIndices])
+# 
+# 
+# postscript("../Figures/ROC_cv10_RF_RND.eps")
+# plot(smooth(ROC_Com_RND),col="red")
+# lines(smooth(ROC_Net_RND), col="green")
+# lines(smooth(ROC_gwawa_RND), col="orange")
+# lines(smooth(ROC_funseq_RND), col="blue")
+# dev.off()
+# 
+# postscript("../Figures/ROC_cv10_RF_TSS.eps")
+# plot(smooth(ROC_Com_TSS),col="red")
+# lines(smooth(ROC_Net_TSS), col="green")
+# lines(smooth(ROC_gwawa_TSS), col="orange")
+# lines(smooth(ROC_funseq_TSS), col="blue")
+# dev.off()
+# 
+# postscript("../Figures/ROC_cv10_RF_1k.eps")
+# plot(smooth(ROC_Com_1k),col="red")
+# lines(smooth(ROC_Net_1k), col="green")
+# lines(smooth(ROC_gwawa_1k), col="orange")
+# lines(smooth(ROC_funseq_1k), col="blue")
+# dev.off()
+# 
+# smooth(ROC_Com_RND)
+# smooth(ROC_Net_RND)
+# smooth(ROC_gwawa_RND)
+# smooth(ROC_funseq_RND)
+# CI_Com_RND <- ci.auc(smooth(ROC_Com_RND))
+# CI_Net_RND <- ci.auc(smooth(ROC_Net_RND))
+# CI_gwawa_RND <- ci.auc(smooth(ROC_gwawa_RND))
+# CI_funseq_RND <- ci.auc(smooth(ROC_funseq_RND))
+# 
+# CI_Com_TSS <- ci.auc(smooth(ROC_Com_TSS))
+# CI_Net_TSS <- ci.auc(smooth(ROC_Net_TSS))
+# CI_gwawa_TSS <- ci.auc(smooth(ROC_gwawa_TSS))
+# CI_funseq_TSS <- ci.auc(smooth(ROC_funseq_TSS))
+# 
+# CI_Com_1k <- ci.auc(smooth(ROC_Com_1k))
+# CI_Net_1k <- ci.auc(smooth(ROC_Net_1k))
+# CI_gwawa_1k <- ci.auc(smooth(ROC_gwawa_1k))
+# CI_funseq_1k <- ci.auc(smooth(ROC_funseq_1k))
+# 
+# Com_auc_set <- c(smooth(ROC_Com_RND)$auc, smooth(ROC_Com_TSS)$auc, smooth(ROC_Com_1k)$auc)
+# Net_auc_set <- c(smooth(ROC_Net_RND)$auc, smooth(ROC_Net_TSS)$auc, smooth(ROC_Net_1k)$auc)
+# gwawa_auc_set <- c(smooth(ROC_gwawa_RND)$auc, smooth(ROC_gwawa_TSS)$auc, smooth(ROC_gwawa_1k)$auc)
+# funseq_auc_set <- c(smooth(ROC_funseq_RND)$auc, smooth(ROC_funseq_TSS)$auc, smooth(ROC_funseq_1k)$auc)
+# 
+# Com_up_set <- c(max(CI_Com_RND),max(CI_Com_TSS),max(CI_Com_1k))
+# Net_up_set <- c(max(CI_Net_RND),max(CI_Net_TSS),max(CI_Net_1k))
+# gwawa_up_set <- c(max(CI_gwawa_RND),max(CI_gwawa_TSS),max(CI_gwawa_1k))
+# funseq_up_set <- c(max(CI_funseq_RND),max(CI_funseq_TSS),max(CI_funseq_1k))
+# 
+# com_mean <- cbind(funseq_auc_set, gwawa_auc_set, Net_auc_set, Com_auc_set)
+# com_se <- cbind(funseq_up_set, gwawa_up_set, Net_up_set, Com_up_set)
+# postscript("../Figures/AUC_cv10_RF.eps")
+# barCenters <- barplot(com_mean, ylim=c(0,1), xaxt="n", beside=TRUE)
+# labCenters <- barCenters[2,]
+# axis(1, at=labCenters,labels=c("FunSeq", "GWAWA", "Network","Combined"))
+# arrows(barCenters, com_mean, barCenters,
+#        com_se, lwd = 1.5, angle = 90,
+#        code = 2, length = 0.1)
+# dev.off()
+# 
+# #######################
+# #feature rank
+# feature_rank_rnd <- varImp(rfFit_Com_RND)$importance$Overall
+# feature_rank_tss <- varImp(rfFit_Com_TSS)$importance$Overall
+# feature_rank_rnd <- varImp(rfFit_Com_1k)$importance$Overall
+# temp_rank_rnd <- rank(-feature_rank_rnd)
+# temp_rank_tss <- rank(-feature_rank_tss)
+# temp_rank_1k <- rank(-feature_rank_1k)
+# com_rank <- (temp_rank_rnd + temp_rank_tss + temp_rank_1k)/3
+# names(com_rank) <- row.names(feature_rank_rnd)
+# com_rank_frame <- cbind(temp_rank_rnd, temp_rank_tss,temp_rank_1k, com_rank)
+# row.names(com_rank_frame) <- row.names(varImp(rfFit_Com_RND)$importance)
+# write.table(com_rank_frame,"All_Features_importance_rank.txt",quote=F, sep="\t")
+# 
+# save(list=ls(),file="Aug22_CV_classification.RData")
+# 
+# 
+# 
+# ###########################################################
+# #test different ROC curves
+# roc.test(smooth(ROC_Com_RND), smooth(ROC_Net_RND))#8.652e-07
+# roc.test(smooth(ROC_Com_RND), smooth(ROC_gwawa_RND))#0.01687
+# roc.test(smooth(ROC_Com_RND), smooth(ROC_funseq_RND))#8.603e-10
+# 
+# roc.test(smooth(ROC_Com_TSS), smooth(ROC_Net_TSS))#0.4463
+# roc.test(smooth(ROC_Com_TSS), smooth(ROC_gwawa_TSS))#2.985e-11
+# roc.test(smooth(ROC_Com_TSS), smooth(ROC_funseq_TSS))#2.2e-16
+# 
+# roc.test(smooth(ROC_Com_1k), smooth(ROC_Net_1k))#0.9693
+# roc.test(smooth(ROC_Com_1k), smooth(ROC_gwawa_1k))#2.625e-13
+# roc.test(smooth(ROC_Com_1k), smooth(ROC_funseq_1k))#2.2e-16
